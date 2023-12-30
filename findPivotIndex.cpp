dbf19ee2848aff58b4ccc9a191d945d0c7d8ee21
AniketGhodake <ghodakeaniket27@gmail.com>
2023-12-30 15:47:49 +0530

    30 Dec 2023

--- findPivotIndex.cpp
+++ findPivotIndex.cpp
@@ -10,8 +10,9 @@
 //                 lsum = lsum + nums[j];
 //             }
 
-            // for(int j = i+1; j < nums.size(); j++) //------- Right answer but takes too much memory and time
-//                 rsum = rsum + nums[j]; 
+            // for(int j = i+1; j < nums.size(); j++)//----  Right answer but takes too much memory and time
+//                 rsum = rsum + nums[j];              
+                                                           // ------   Runtime 1979ms and memory 31.5MB
 //             }
 
 //             if(lsum == rsum){
@@ -21,5 +22,26 @@
 //         return -1;
 //     }
 // };
+class Solution {
+public:
+    int pivotIndex(vector<int>& nums) {
+        vector<int>lsum(nums.size(),0);
+        vector<int>rsum(nums.size(),0);
 
+        for(int i = 1; i < nums.size(); i++){
+            lsum[i] = lsum[i-1] + nums[i-1];
+        }
+
+        for(int i = nums.size()-2; i >= 0; --i){    // ----------    runtime 39ms and memory 32.6 MB
+            rsum[i] = rsum[i+1] + nums[i+1];
+        }
+
+        for(int i = 0; i < nums.size(); ++i){
+            if(lsum[i]==rsum[i]){
+                return i;
+            }
+        }
+        return -1;
+    }
+};
 
