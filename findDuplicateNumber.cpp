dc07407b8dfaf1ee52a9b5ca9557fd34e644c40b
Aniket Sunil Ghodake <74956511+AniGhodake@users.noreply.github.com>
2024-01-02 22:13:32 +0530

    Update findDuplicateNumber.cpp

--- findDuplicateNumber.cpp
+++ findDuplicateNumber.cpp
@@ -7,9 +7,9 @@ public:
     int findDuplicate(vector<int>& nums) {
         unordered_map<int,int>hash;
 
-        for(int i = 0; i < nums.size(); i++){
-            hash[nums[i]]++;
-        }
+        // for(int i = 0; i < nums.size(); i++){
+        //     hash[nums[i]]++;
+        // }
 
         for(int i = 0; i < nums.size(); i++){
             if(hash[nums[i]]>1){
