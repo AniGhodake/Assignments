94fa1846f143fb5471af27134e9ec000ffb1e3d7
Aniket Sunil Ghodake <74956511+AniGhodake@users.noreply.github.com>
2024-01-03 21:17:05 +0530

    Update keyPair.cpp

--- keyPair.cpp
+++ keyPair.cpp
@@ -3,32 +3,32 @@
 using namespace std;
 #include<math.h>
 
-class Solution{
-public:	
-	// Function to check if array has 2 elements
-	// whose sum is equal to the given value
-	bool hasArrayTwoCandidates(int arr[], int n, int x) {
-	    // code here
-    sort(arr,arr + n);
+// class Solution{
+// public:	
+// 	// Function to check if array has 2 elements
+// 	// whose sum is equal to the given value
+// 	bool hasArrayTwoCandidates(int arr[], int n, int x) {
+// 	    // code here
+//     sort(arr,arr + n);
 	   
-	   int csum,left=0,right = n-1;
-	   while(left<right){
-	       csum = arr[left] + arr[right];
-	       if(csum == x){
-	           return true;
-	       }
-	       else if(csum<x){
-	           left++;
-	       }
-	       else{
-	           right--;
-	       }
-	   }
+// 	   int csum,left=0,right = n-1;
+// 	   while(left<right){
+// 	       csum = arr[left] + arr[right];
+// 	       if(csum == x){
+// 	           return true;
+// 	       }
+// 	       else if(csum<x){
+// 	           left++;
+// 	       }
+// 	       else{
+// 	           right--;
+// 	       }
+// 	   }
 	   
-	   return false;
+// 	   return false;
 	   
-	}
-};
+// 	}
+// };
 
 //{ Driver Code Starts.
 
